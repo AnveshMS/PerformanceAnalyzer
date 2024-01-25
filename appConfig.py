0899cf1fdb7617570cb7d86d463e752ec14201bf
AnveshMS <141767439+AnveshMS@users.noreply.github.com>
2024-01-25 22:19:47 +0530

    Update appConfig.py

--- appConfig.py
+++ appConfig.py
@@ -15,8 +15,8 @@ def fetchKey(key):
     Raises:
         Exception: If the configuration key is not found or an error occurs while fetching the value.
     """
-    # connection_str = os.getenv("APP_CONFIG_CONNECTION_STRING")
-    connection_str = 'Endpoint=https://perf-appconfig.azconfig.io;Id=fWd0;Secret=DwVO/onZY/Cz/ZP+SQxse/NR5Fa3wLQIXzwEGGnPVwM='
+    connection_str = os.getenv("APP_CONFIG_CONNECTION_STRING")
+    # connection_str = 'Endpoint=https://perf-appconfig.azconfig.io;Id=fWd0;Secret=DwVO/onZY/Cz/ZP+SQxse/NR5Fa3wLQIXzwEGGnPVwM='
     client = AzureAppConfigurationClient.from_connection_string(connection_str)
     config_setting = client.get_configuration_setting(key=key)
     return config_setting.value
